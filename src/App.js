dfe3b1fe04e4bbf2e025b4832c6a88c065705232
K-JunHo <kalmia6863@gmail.com>
2025-08-18 18:43:14 +0900

    Practice.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Button from "./components/Button";
+import styles from "./css/App.module.css";
+
+function App() {
+	return (
+		<div>
+			<h1 className={styles.title}>Welcome back!</h1>
+			<Button text={"Click me"} />
+		</div>
+	);
+}
+
+export default App;
