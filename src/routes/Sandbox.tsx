f90fa1c2b07c7baa37a54634a35d0c1e62a51330
K-JunHo <kalmia6863@gmail.com>
2025-08-25 11:50:44 +0900

    Practice: useNotification.

--- src/routes/Sandbox.tsx
+++ src/routes/Sandbox.tsx
@@ -1,29 +1,7 @@
-import { useFullScreen } from "hooks/useFullScreen";
-import logoImg from "../resources/moons-rainbow.png";
-
 function SandboxRouter() {
-	const { isFull, imgRef, triggerFullEvent, exitFullEvent } = useFullScreen();
 	return (
 		<div>
 			<h1>Hello</h1>
-			<div ref={imgRef}>
-				<img
-					src={logoImg}
-					alt="moons-rainbow.png"
-				/>
-				<button
-					onClick={exitFullEvent}
-					style={{ display: isFull ? "block" : "none" }}
-				>
-					Exit Full-Screen
-				</button>
-			</div>
-			<button
-				onClick={triggerFullEvent}
-				style={{ display: isFull ? "none" : "block" }}
-			>
-				View Full-Screen
-			</button>
 		</div>
 	);
 }
